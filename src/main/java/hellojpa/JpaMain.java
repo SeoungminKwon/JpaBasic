6326f4f7a54f09cbd39e89de6787158445dcf9a6
SeoungminKwon <tjdalsrnjs10@gmail.com>
2023-12-01 23:50:42 +0900

    2-1 Setting

---
+++ src/main/java/hellojpa/JpaMain.java
@@ -0,0 +1,17 @@
+package hellojpa;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class JpaMain {
+    public static void main(String[] args) {
+        //UnitName은 persistence.xml파일의 unitName
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+        EntityManager em = emf.createEntityManager();
+        //code작성 부분
+        em.close();
+        emf.close();
+    }
+
+}
