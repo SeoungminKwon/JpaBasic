9b243d29457f5a59b119e085d13f3772bdb884bb
SeoungminKwon <tjdalsrnjs10@gmail.com>
2023-12-03 17:39:55 +0900

    2-Practice6

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -4,6 +4,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import java.util.List;
 
 public class JpaMain {
     public static void main(String[] args) {
@@ -16,9 +17,12 @@ public class JpaMain {
         tx.begin();
 
         try{
-            Member member = em.find(Member.class, 1L);
-            member.setName("userB");
-
+            Member member = new Member();
+            member.setId(100L);
+            member.setName("userTest");
+            System.out.println("=== before ===");
+            em.persist(member);
+            System.out.println("=== after ===");
             tx.commit();
         }catch (Exception e){
             tx.rollback();
