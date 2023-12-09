2c64e53150c38ad184e8b551fb3592a7387f41b0
SeoungminKwon <tjdalsrnjs10@gmail.com>
2023-12-09 16:48:08 +0900

    error solve

--- src/main/java/hellojpa/Item.java
+++ src/main/java/hellojpa/Item.java
@@ -1,26 +1,24 @@
 package hellojpa;
 
 import javax.persistence.*;
-import java.util.ArrayList;
-import java.util.List;
 
 @Entity
-public class Product {
-
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+@DiscriminatorColumn
+public class Item {
     @Id
     @GeneratedValue
     private Long id;
 
     private String name;
+    private int price;
 
-    @OneToMany(mappedBy = "product")
-    private List< MemberProduct > memberProducts;
 
-    public long getId() {
+    public Long getId() {
         return id;
     }
 
-    public void setId(long id) {
+    public void setId(Long id) {
         this.id = id;
     }
 
@@ -30,5 +28,13 @@ public class Product {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
     }
 }
--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -17,17 +17,18 @@ public class JpaMain {
         tx.begin();
 
         try{
+            Movie movie = new Movie();
+            movie.setDirector("aaaa");
+            movie.setActor("bbbbb");
+            movie.setName("바람과 함께 사라지다.");
+            movie.setPrice(10000);
 
-            Member member = new Member();
-            member.setUsername("userA");
+            em.persist(movie);
 
-            em.persist(member);
-
-            Team team = new Team();
-            team.setName("TeamA");
-            team.getMembers().add(member);
-
-            em.persist(team);
+            em.flush();
+            em.clear();
+            Movie findMovie = em.find(Movie.class, movie.getId());
+            System.out.println("findMovie = " + findMovie);
 
             tx.commit();
         }catch (Exception e){
