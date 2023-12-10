02e27a082ac73ab7401c8200f864d82ee5644c8a
SeoungminKwon <tjdalsrnjs10@gmail.com>
2023-12-10 17:24:43 +0900

    Practice30

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -17,11 +17,21 @@ public class JpaMain {
 
         try{
 
-            Member member = new Member();
-            member.setUsername("hello");
-            member.setHomeAddress(new Address("city", "street", "10000"));
-            member.setWorkPeriod(new Period());
-            em.persist(member);
+            Address address = new Address("city", "street", "10000");
+
+            Member member1 = new Member();
+            member1.setUsername("member1");
+            member1.setHomeAddress(address);
+            em.persist(member1);
+
+            Member member2 = new Member();
+            member2.setUsername("member2");
+            member2.setHomeAddress(address);
+            em.persist(member2);
+
+            member1.getHomeAddress().setCity("newCity");
+
+
 
             tx.commit();
         }catch (Exception e){
