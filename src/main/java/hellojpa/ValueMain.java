a85854bef1a895640b2dfcf6bbf476b7fe976fe5
SeoungminKwon <tjdalsrnjs10@gmail.com>
2023-12-10 16:39:34 +0900

    Practice27

--- src/main/java/hellojpa/ValueMain.java
+++ src/main/java/hellojpa/ValueMain.java
@@ -2,9 +2,11 @@ package hellojpa;
 
 public class ValueMain {
     public static void main(String[] args) {
-        int a = 10;
-        int b = a; // 타입이 복사되는 것임 공유 x
-        a = 20;
+
+        Integer a = new Integer(10);
+        Integer b = a;
+
+
         System.out.println("a = " + a);
         System.out.println("b = " + b);
     }
